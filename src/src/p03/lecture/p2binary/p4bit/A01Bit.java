44085d502a4caee74ee64101df267b3f9f356a47
seobei <sys1774@nate.com>
2021-03-30 16:07:19 +0900

    If

--- src/src/p03/lecture/p2binary/p4bit/A01Bit.java
+++ src/src/p03/lecture/p2binary/p4bit/A01Bit.java
@@ -1,5 +1,16 @@
 package src.p03.lecture.p2binary.p4bit;
 
 public class A01Bit {
-
+public static void main(String[] args) {
+	// and & : 같은 자리의 비트가 둘 다 1이면, 1, 나머지 0
+	//or |   : 같은 자리의 비트가 둘 다 0이면, 0, 나머지 1
+	// xor ^ : 같은 자리의 비트가 다르면 1, 같으면 0
+	// not ~ : 0 --> 1, 1 --> 0
+	
+	// and &
+	int a = 99; // 2진법 : 1100011
+	int b = 999;// 2진법 : 001111100111
+	
+	
+}
 }
