c510dcf68d318a39e5928d263cc8bb1a5cdbfaae
seobei <sys1774@nate.com>
2021-03-31 14:15:39 +0900

    Multiplication

--- src/p04/textbook/ForMultiplicationExample2.java
+++ src/p04/textbook/ForMultiplicationExample2.java
@@ -45,7 +45,21 @@ public class ForMultiplicationExample2 {
 			}
 			System.out.println();
 		}
-
+		/*
+		     * 
+		    ** 
+		   *** 
+		  ****
+		 ***** 
+		 */
+		for(int i =0; i <5; i++) {
+			for(int k=0; k< (4-i); k++) {
+			System.out.print(" ");
+			}
+			for (int j=0; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	  }
 	}
-
-}
