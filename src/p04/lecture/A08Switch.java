15c85ffb6e61b4a9d9940a62ad66d5118f7ab122
seobei <sys1774@nate.com>
2021-03-31 09:35:31 +0900

    switch

--- src/p04/lecture/A08Switch.java
+++ src/p04/lecture/A08Switch.java
@@ -4,6 +4,8 @@ public class A08Switch {
 
 	public static void main(String[] args) {
 		// switch case : 값이 일치하는 case문부터 실행
+		// 만약 일치하는 case가 없으면 default부터 실행
+		// 사용할 수 있는 type: byte, short, char, int, String, Enum
 		
 		int a  = 2;
 		
