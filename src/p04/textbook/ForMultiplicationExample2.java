dbc9249de211ba92fb4d7b149b10844602f7fe24
seobei <sys1774@nate.com>
2021-03-31 14:11:42 +0900

    Multiplication

---
+++ src/p04/textbook/ForMultiplicationExample2.java
@@ -0,0 +1,51 @@
+package p04.textbook;
+
+public class ForMultiplicationExample2 {
+
+	public static void main(String[] args) {
+		/*
+		 * 
+		 **
+		 ***
+		 ****
+		 ***** 
+		 */
+		for(int i = 0; i<5; i++) {
+			for(int j =0; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		/*
+		 ***** 
+		 ****
+		 ***
+		 **
+		 *
+		 */
+		for(int i=0; i<5; i++) {
+			for(int j=5; j>i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		/*
+		 *****
+		  **** 
+		   *** 
+		    **
+		     * 
+		 */
+		for(int i=0; i<5; i++) {
+			for(int k=0; k<i; k++) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<(5-i); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
