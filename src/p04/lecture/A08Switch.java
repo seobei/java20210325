541d0b69f44b1892bbed046c2c283bee5caaccc2
seobei <sys1774@nate.com>
2021-03-31 09:23:44 +0900

    switch

---
+++ src/p04/lecture/A08Switch.java
@@ -0,0 +1,22 @@
+package p04.lecture;
+
+public class A08Switch {
+
+	public static void main(String[] args) {
+		// switch case : 값이 일치하는 case문부터 실행
+		
+		int a  = 2;
+		
+		switch (a) {
+		case 1:
+			System.out.println("케이스 1");
+			break;
+		case 2:
+			System.out.println("케이스 2");
+			break;
+		case 3:
+			System.out.println("케이스 3");
+			break;
+		}
+	}
+}
