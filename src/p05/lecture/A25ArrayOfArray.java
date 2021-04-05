81943add2d00755169c3712b85c45f4a00865d04
seobei <sys1774@nate.com>
2021-04-05 12:24:52 +0900

    as

--- src/p05/lecture/A25ArrayOfArray.java
+++ src/p05/lecture/A25ArrayOfArray.java
@@ -1,11 +1,18 @@
 package p05.lecture;
 
+import java.util.Arrays;
+
 public class A25ArrayOfArray {
 
 	public static void main(String[] args) {
 		// 그림:09.배열의 배열.png
 		int[][] arr = new int[2][3];
+		arr[1][1] = 9;
+		arr[0][2] = 8;
 		
+		for(int[] elem : arr) {
+			System.out.println(Arrays.toString(elem));
+		}
 
 	}
 
