8c4ae0fae706d9c4173367803c5fba09f89ffe0c
seobei <sys1774@nate.com>
2021-04-16 15:42:38 +0900

    09

---
+++ src/p09/lecture/A01NestedClass.java
@@ -0,0 +1,18 @@
+package p09.lecture;
+
+public class A01NestedClass {
+	// 필드
+	// 생성자
+	// 메소드
+
+	// 클래스
+	class InnerClass {
+		// 필드
+		// 생성자
+		// 메소드
+	}
+
+	static class StaticClass {
+
+	}
+}
---
+++ src/p09/lecture/A03Access.java
@@ -0,0 +1,11 @@
+package p09.lecture;
+
+public class A03Access {
+	void instanceMethod() {
+		staticMethod();
+	}
+
+	static void staticMethod() {
+		
+	}
+}
